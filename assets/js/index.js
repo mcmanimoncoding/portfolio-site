5d1368b6fd28d6b21e6a5f232e8d461efb44eb39
Daniel M <mcmanimoncoding@gmail.com>
2019-04-13 23:08:43 -0700

    Animations

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,22 @@
-var sal = require('sal.js');
+// Scroll function courtesy of Scott Dowding; http://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling
+$(document).ready(function() {
+  // Check if element is scrolled into view
+  function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window
+).height();
 
-sal({
-    threshold: 1,
-    once: false,
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  }
+  // If element is scrolled into view, fade it in
+  $(window).scroll(function() {
+    $('.scroll-animations .animated').each(function() {
+      if (isScrolledIntoView(this) === true) {
+        $(this).addClass('fadeInLeft');
+      }
+    });
   });
-
-var express = require("express");
-var handlebars = require("mysql");
+});
