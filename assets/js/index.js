2cc51e5490559592ae49ce655abd07b4a809b354
Daniel M <mcmanimoncoding@gmail.com>
2019-04-16 19:10:35 -0700

    resume

--- assets/js/index.js
+++ assets/js/index.js
@@ -24,14 +24,14 @@
 // });
 
 
-function toggle() {
-  var button = document.querySelector('.toggle');
-  var overlay = document.querySelector('.glass');
-  if (overlay.className === 'glass down') {
-    overlay.className = 'glass up';
-    button.innerText = '-';
-  } else {
-    overlay.className = 'glass down';
-    button.innerText = '+';
-  }
-}
+// function toggle() {
+//   var button = document.querySelector('.toggle');
+//   var overlay = document.querySelector('.glass');
+//   if (overlay.className === 'glass down') {
+//     overlay.className = 'glass up';
+//     button.innerText = '-';
+//   } else {
+//     overlay.className = 'glass down';
+//     button.innerText = '+';
+//   }
+// }
